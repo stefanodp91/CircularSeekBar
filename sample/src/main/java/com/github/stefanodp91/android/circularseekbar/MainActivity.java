7c22d83012e4316920fe50a3e184147741025143
Stefano De Pascalis <stefanodp91@gmail.com>
2019-06-14 18:34:58 +0200

    https://issues.sonatype.org/browse/OSSRH-49355

--- sample/src/main/java/com/github/stefanodp91/android/circularseekbar/MainActivity.java
+++ sample/src/main/java/com/github/stefanodp91/android/circularseekbar/MainActivity.java
@@ -1,15 +1,26 @@
 package com.github.stefanodp91.android.circularseekbar;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatTextView mProgress;
+
+//    for your attention:
+//    https://issues.sonatype.org/browse/OSSRH-49355
+//    https://github.com/stefanodp91/CircularSeekBar
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mProgress = findViewById(R.id.progress);
 
         CircularSeekBar seekBar = findViewById(R.id.seek);
         seekBar.setOnRoundedSeekChangeListener(new OnCircularSeekBarChangeListener() {
@@ -21,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChange(CircularSeekBar CircularSeekBar, float progress) {
                 Log.d("progress", "" + progress);
+                mProgress.setText(String.valueOf(progress));
             }
 
             /**
